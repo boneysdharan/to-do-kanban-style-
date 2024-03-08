c8502e86b1a3469126e0fc585f4b3daf4e80433f
boneysdharan <118093595+boneysdharan@users.noreply.github.com>
2024-03-08 09:58:02 +0530

    Add files via upload

---
+++ src/redux/reducer.js
@@ -0,0 +1,56 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = [];
+
+const addTodoReducer = createSlice({
+  name: "todos",
+  initialState,
+  reducers: {
+    //here we will write our reducer
+    //Adding todos
+    addTodos: (state, action) => {
+      state.push(action.payload);
+      localStorage.setItem("todo_list",JSON.stringify(state))
+      return state;
+    },
+    //remove todos
+    removeTodos: (state, action) => {
+      localStorage.setItem("todo_list",JSON.stringify(state.filter((item) => item.id !== action.payload)))
+      return state.filter((item) => item.id !== action.payload);
+    },
+    //update todos
+    updateTodos: (state, action) => {
+      return state.map((todo) => {
+        if (todo.id === action.payload.id) {
+          return {
+            ...todo,
+            item: action.payload.item,
+            ...localStorage.setItem("todo_list",JSON.stringify(action.payload.item)),
+          };
+        }
+        return todo;
+      });
+    },
+    //completed
+    completeTodos: (state, action) => {
+      return state.map((todo) => {
+        if (todo.id === action.payload) {
+          return {
+            ...todo,
+            completed: true,
+            ...localStorage.setItem("todo_list",JSON.stringify(todo)),
+          };
+        }
+        return todo;
+      });
+    },
+  },
+});
+
+export const {
+  addTodos,
+  removeTodos,
+  updateTodos,
+  completeTodos,
+} = addTodoReducer.actions;
+export const reducer = addTodoReducer.reducer;
